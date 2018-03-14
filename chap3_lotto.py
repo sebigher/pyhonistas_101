674b0f23e342c13c9e5443678ed2d803b6a548c6
sebigher <sebigher@gmail.com>
2018-03-14 08:35:37 +0200

    initial lotto commit

---
+++ chap3_lotto.py
@@ -0,0 +1,16 @@
+import random
+
+# stupid draws
+number1 = random.randint(1, 49)
+number2 = random.randint(1, 49)
+number3 = random.randint(1, 49)
+number4 = random.randint(1, 49)
+number5 = random.randint(1, 49)
+number6 = random.randint(1, 49)
+
+print("#1:", number1)
+print("#2:", number2)
+print("#3:", number3)
+print("#4:", number4)
+print("#5:", number5)
+print("#6:", number6)
